e3ae691ea78421c63ee42778278acec3d88b6798
JMM889901 <ipodmcwilliam@aol.com>
2023-03-03 15:16:30 +0000

    countertops

--- core/src/com/devcharles/piazzapanic/components/Powerups/timeFreezeBoostComponent.java
+++ core/src/com/devcharles/piazzapanic/components/Powerups/timeFreezeBoostComponent.java
@@ -3,6 +3,10 @@ package com.devcharles.piazzapanic.components.Powerups;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
+/*
+ * Searched for in Unimplemented
+ * Given in {@link componentsystems.PowerupSpawnSystem}
+ */
 public class timeFreezeBoostComponent implements Component, Poolable {
     public static float timeMax = 5;
     public float timeHad = 0;
