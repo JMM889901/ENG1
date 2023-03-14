6424ed678c0e4c2f161aaa807e7e7fea5ae3a34a
Joss Davis <95944691+JoO0oss@users.noreply.github.com>
2023-03-14 16:10:03 +0000

    Initiliased food component in test.

--- tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
+++ tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
@@ -13,9 +13,10 @@ public class testPlayerControlSystem {
         PlayerControlSystem testPlayerControlSystem = new PlayerControlSystem(new KeyboardInput(), null); // Do I need to add a new Pooledengine here?
         PlayerComponent testPlayerComponent = new PlayerComponent();
         EntityFactory testEntityFactory = new EntityFactory(null, null);
+        
         FoodComponent testFoodType = new FoodComponent();
-        testFoodType.FoodType = 1; //TODO - Joss please look
-        testEntityFactory.createFood(new , 0.00f);
+        testFoodType.type = FoodType.unformedPatty; // From the FoodType: unformedPatty(1)
+        testEntityFactory.createFood(testFoodType.type);
         testPlayerControlSystem.processEntity(null, 0);
     }
 }
