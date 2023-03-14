f2b4f7d6eae47ab3a555b2fbbca9be966ae25032
Emily <ehd507@york.ac.uk>
2023-03-14 15:04:04 +0000

    Example test

--- tests/src/com/devcharles/piazzapanic/testTests.java
+++ tests/src/com/devcharles/piazzapanic/testTests.java
@@ -1,8 +1,17 @@
 package com.devcharles.piazzapanic;
 import org.junit.*;
+import com.devcharles.piazzapanic.GameScreen;
 public class testTests {
    @Test
    public void test() {
        Assert.assertTrue(true);
+   }
+   @Test
+   /**
+    * 
+    */
+   public void virtualHeightTest() {
+        PiazzaPanic panic = new PiazzaPanic();
+        Assert.assertTrue(panic.VIRTUAL_HEIGHT == 20f);
    }
 }
