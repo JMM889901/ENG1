b10f275737e12f51373f82d086f85c91fe5c9e25
Emily <ehd507@york.ac.uk>
2023-03-14 16:32:58 +0000

    Getting there...

--- tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
+++ tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
@@ -8,6 +8,10 @@ import com.devcharles.piazzapanic.components.FoodComponent.FoodType;
 import com.devcharles.piazzapanic.input.KeyboardInput;
 import com.devcharles.piazzapanic.utility.EntityFactory;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.math.Vector2;
+
 
 
 public class testPlayerControlSystem {
@@ -15,7 +19,10 @@ public class testPlayerControlSystem {
     public void testPlayerPickUpItem(){
         PlayerControlSystem testPlayerControlSystem = new PlayerControlSystem(new KeyboardInput(), null); // Do I need to add a new Pooledengine here?
         PlayerComponent testPlayerComponent = new PlayerComponent();
-        EntityFactory testEntityFactory = new EntityFactory(null, null);
+        PooledEngine engine = new PooledEngine();
+        World world = new World(new Vector2(0, 0), true);
+
+        EntityFactory testEntityFactory = new EntityFactory(engine, world);
         
         FoodComponent testFoodType = new FoodComponent();
         testFoodType.type = FoodType.unformedPatty; // From the FoodType: unformedPatty(1)
@@ -23,6 +30,6 @@ public class testPlayerControlSystem {
         testPlayerControlSystem.playerComponent.pickUp = false; // Set the pickUp flag to false so it can pick something up
         testPlayerControlSystem.processEntity(testFoodEntity, 0);
         ControllableComponent testControllableComponent = new ControllableComponent();
-        
+        Assert.assertEquals(testControllableComponent.currentFood.pop(), testFoodType);
     }
 }
