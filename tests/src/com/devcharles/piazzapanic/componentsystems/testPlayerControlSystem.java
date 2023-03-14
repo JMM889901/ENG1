879c91689596ff0142c17119a99b55147102a58d
Emily <ehd507@york.ac.uk>
2023-03-14 16:24:48 +0000

    help me

--- tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
+++ tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
@@ -1,11 +1,14 @@
 package com.devcharles.piazzapanic.componentsystems;
 import org.junit.*;
 
+import com.devcharles.piazzapanic.components.ControllableComponent;
 import com.devcharles.piazzapanic.components.FoodComponent;
 import com.devcharles.piazzapanic.components.PlayerComponent;
 import com.devcharles.piazzapanic.components.FoodComponent.FoodType;
 import com.devcharles.piazzapanic.input.KeyboardInput;
 import com.devcharles.piazzapanic.utility.EntityFactory;
+import com.badlogic.ashley.core.Entity;
+
 
 public class testPlayerControlSystem {
     @Test
@@ -16,7 +19,10 @@ public class testPlayerControlSystem {
         
         FoodComponent testFoodType = new FoodComponent();
         testFoodType.type = FoodType.unformedPatty; // From the FoodType: unformedPatty(1)
-        testEntityFactory.createFood(testFoodType.type);
-        testPlayerControlSystem.processEntity(null, 0);
+        Entity testFoodEntity = testEntityFactory.createFood(testFoodType.type);
+        testPlayerControlSystem.playerComponent.pickUp = false; // Set the pickUp flag to false so it can pick something up
+        testPlayerControlSystem.processEntity(testFoodEntity, 0);
+        ControllableComponent testControllableComponent = new ControllableComponent();
+        
     }
 }
