553e0ea6847055c72ef695ee6a8e614700b43f2e
Joss Davis <95944691+JoO0oss@users.noreply.github.com>
2023-03-14 15:10:08 +0000

    First (second) working test.

--- tests/src/com/devcharles/piazzapanic/testTests.java
+++ tests/src/com/devcharles/piazzapanic/testTests.java
@@ -1,8 +1,17 @@
 package com.devcharles.piazzapanic;
+
 import org.junit.*;
+
+// Import libgdx desktop to test that.
+//import com.devcharles.piazzapanic.DesktopLauncher;
+import com.devcharles.piazzapanic.PiazzaPanic;
+
 public class testTests {
    @Test
    public void test() {
        Assert.assertTrue(true);
+
+       PiazzaPanic piazzaPanic = new PiazzaPanic();
+       Assert.assertEquals(piazzaPanic.VIRTUAL_HEIGHT, 20f, 0.01);  // Use the delta to specify an acceptable error margin.
    }
 }
