9f420ab4b91fde445196cb06f4289936e25a3a28
Emily <ehd507@york.ac.uk>
2023-03-17 14:20:44 +0000

    add file test

--- tests/src/com/devcharles/piazzapanic/testAssets.java
+++ tests/src/com/devcharles/piazzapanic/testAssets.java
@@ -1,16 +1,12 @@
 package com.devcharles.piazzapanic;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.junit.*;
+import com.badlogic.gdx.Gdx;
 
 public class testAssets {  
     @Test
     public void testAssetsPresent(){
-        Path path = Paths.get("does-not-exist.txt");
-        Assert.assertFalse(Files.exists(path));
+        // TODO - this returns a NullPointerException and i DONT KNOW WHY
+        Gdx.files.internal("mainMenuImage.png");
     }
     
 }
