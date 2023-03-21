fa69ded6815d42d7aea84e7cb438ba1205735b32
Emily <ehd507@york.ac.uk>
2023-03-21 19:35:20 +0000

    small change

--- tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
+++ tests/src/com/devcharles/piazzapanic/componentsystems/testPlayerControlSystem.java
@@ -1,11 +1,6 @@
 package com.devcharles.piazzapanic.componentsystems;
 
-import static org.mockito.Mockito.mock;
-
 import org.junit.*;
-
-import com.devcharles.piazzapanic.PiazzaPanic;
-import com.devcharles.piazzapanic.emptyGL20;
 import com.devcharles.piazzapanic.testEnvironment;
 import com.devcharles.piazzapanic.components.ControllableComponent;
 import com.devcharles.piazzapanic.components.FoodComponent;
@@ -17,46 +12,42 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.graphics.GL20;
 
 public class testPlayerControlSystem {
     @Test
+    /**
+     * Tests if a player can pick up an item from a countertop
+     */
     public void testPlayerPickUpItem() {
         new testEnvironment();
-
         // Initialise wider systems.
         PlayerControlSystem testPlayerControlSystem = new PlayerControlSystem(new KeyboardInput(), null);// Do I need to
         // add a new PooledEngine here?
         PlayerComponent testPlayerComponent = new PlayerComponent();
-
         PooledEngine engine = new PooledEngine();
         World world = new World(new Vector2(0, 0), true);
-
         engine.addSystem(testPlayerControlSystem);
-
         // Initialise foods (and the entity factory to make said food).
         EntityFactory testEntityFactory = new EntityFactory(engine, world);
         FoodComponent testFoodType = new FoodComponent();
         testFoodType.type = FoodType.unformedPatty; // From the FoodType: unformedPatty(1)
         Entity testFoodEntity = testEntityFactory.createFood(testFoodType.type);
-
         // Initialise a chef.
         Entity chef = testEntityFactory.createCook(0, 0);
-
         chef.add(testPlayerComponent);
-        // testPlayerControlSystem.processEntity_test(chef, 0);
         engine.update(1);
-        testPlayerControlSystem.playerComponent.pickUp = true; // Set the pickUp flag to false so it can pick something
-                                                               // up
-
+        //TODO - add a countertop to pick the item up from. 
+        testPlayerControlSystem.playerComponent.pickUp = true; // Set the pickUp flag to false, allows chef to pickup
         ControllableComponent testControllableComponent = new ControllableComponent();
-        // We need to make the chef actually pick something up! the testFoodEntity we created earlier. 
+        // We need to make the chef actually pick something up! the testFoodEntity we
+        // created earlier.
         testPlayerControlSystem.processEntity_test(testFoodEntity, 0);
         testControllableComponent.currentFood.init(engine);
         testControllableComponent.currentFood.pushItem(testFoodEntity, chef);
         Assert.assertEquals(testControllableComponent.currentFood.pop(), testFoodEntity);
+    }
+    @Test
+    public void testPlayerPutDownItem(){
+
     }
 }
