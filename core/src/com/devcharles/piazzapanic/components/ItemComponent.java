af7a542ea881e2012efd2fe05cffae20888dee62
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2023-04-10 13:18:05 +0100

    Started docstringing component systems.

--- core/src/com/devcharles/piazzapanic/components/ItemComponent.java
+++ core/src/com/devcharles/piazzapanic/components/ItemComponent.java
@@ -3,8 +3,11 @@ package com.devcharles.piazzapanic.components;
 import com.badlogic.ashley.core.Component;
 
 /**
- * This is just given to entities that can be placed on workstations etc. as items (I think).
+ * This is just given to entities that can be carried, placed on workstations etc. as items (I
+ * think).
  */
 public class ItemComponent implements Component {
+    // This is some information about an entity carrying this item. I presume it is null if it is
+    // not being carried.
     public TransformComponent holderTransform;
 }
