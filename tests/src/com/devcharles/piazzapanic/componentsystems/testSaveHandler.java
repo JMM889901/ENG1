6276a27b9f4231ddbf4903a5526096c350b81c9c
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2023-05-02 22:50:37 +0100

    Started writing testSaveHandler.java.

--- tests/src/com/devcharles/piazzapanic/componentsystems/testSaveHandler.java
+++ tests/src/com/devcharles/piazzapanic/componentsystems/testSaveHandler.java
@@ -5,9 +5,17 @@ import org.junit.*;
 public class testSaveHandler {
     @Test
     /**
-     * Test the player can move left and right
+     * Test that the save handler converts game state into the correct string.
      */
     public void testSave() {
+        
+    }
+
+    @Test
+    /**
+     * Test that the save handler converts the correct string into game state.
+     */
+    public void testLoad() {
         
     }
 }
