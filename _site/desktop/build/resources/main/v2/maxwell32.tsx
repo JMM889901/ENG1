a83c49a4e5fb7b9994ed64f9e3a302f214095f5e
Emily <97851770+emilydennison@users.noreply.github.com>
2023-05-03 09:42:03 +0100

    AAAA

--- _site/desktop/build/resources/main/v2/maxwell32.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="maxwell32" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="maxwell32.png" width="32" height="32"/>
- <tile id="0">
-  <properties>
-   <property name="stationID" type="int" value="10"/>
-  </properties>
- </tile>
-</tileset>
--- _site/desktop/build/resources/main/v2/objects.tsx
+++ _site/desktop/build/resources/main/v2/objects.tsx
@@ -26,60 +26,6 @@
    <property name="stationID" type="int" value="1"/>
   </properties>
  </tile>
- <tile id="5">
-  <properties>
-   <property name="inventoryDirection" value="top"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="6">
-  <properties>
-   <property name="inventoryDirection" value="top"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="7">
-  <properties>
-   <property name="inventoryDirection" value="top"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="8">
-  <properties>
-   <property name="inventoryDirection" value="top"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="9">
-  <properties>
-   <property name="inventoryDirection" value="leftDown"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="10">
-  <properties>
-   <property name="inventoryDirection" value="rightDown"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="11">
-  <properties>
-   <property name="inventoryDirection" value="bottom"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="12">
-  <properties>
-   <property name="inventoryDirection" value="bottom"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
- <tile id="13">
-  <properties>
-   <property name="inventoryDirection" value="bottom"/>
-   <property name="stationID" type="int" value="8"/>
-  </properties>
- </tile>
  <tile id="16">
   <properties>
    <property name="stationID" type="int" value="2"/>
@@ -158,40 +104,6 @@
  <tile id="33">
   <properties>
    <property name="stationID" type="int" value="7"/>
-  </properties>
- </tile>
- <tile id="34">
-  <properties>
-   <property name="stationID" value="8"/>
-  </properties>
- </tile>
- <tile id="35">
-  <properties>
-   <property name="stationID" value="8"/>
-  </properties>
- </tile>
- <tile id="36">
-  <properties>
-   <property name="ingredientType" type="int" value="14"/>
-   <property name="stationID" type="int" value="6"/>
-  </properties>
- </tile>
- <tile id="37">
-  <properties>
-   <property name="ingredientType" type="int" value="17"/>
-   <property name="stationID" type="int" value="6"/>
-  </properties>
- </tile>
- <tile id="39">
-  <properties>
-   <property name="ingredientType" type="int" value="24"/>
-   <property name="stationID" type="int" value="6"/>
-  </properties>
- </tile>
- <tile id="40">
-  <properties>
-   <property name="ingredientType" type="int" value="18"/>
-   <property name="stationID" type="int" value="6"/>
   </properties>
  </tile>
 </tileset>
